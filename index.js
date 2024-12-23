2982b5f5ac9e8d0e16672bf71ccc08cf8e575148
crisdats <147233981+crisdats@users.noreply.github.com>
2024-12-23 09:13:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const path = require('path');
 const axios = require('axios');
 const chalk = require('chalk');
 const figlet = require('figlet');
-const readline = require('readline');
 const FileType = require('file-type');
 const { exec } = require('child_process');
 const { Boom } = require('@hapi/boom');
 const NodeCache = require('node-cache');
 const PhoneNumber = require('awesome-phonenumber');
-const { default: makeWASocket, useMultiFileAuthState, Browsers, DisconnectReason, makeInMemoryStore, makeCacheableSignalKeyStore, proto, getAggregateVotesInPollMessage } = require('@whiskeysockets/baileys');
+const { default: makeWASocket, useMultiFileAuthState, Browsers, DisconnectReason, makeInMemoryStore, makeCacheableSignalKeyStore, proto } = require('@whiskeysockets/baileys');
 
-let phoneNumber = "628895154319";
-const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code");
-const useMobile = process.argv.includes("--mobile");
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const phoneNumber = "628895154319"; // Masukkan nomor telepon di sini
+const pairingCode = false; // Tidak memerlukan pairing code
+const useMobile = false; // Tidak menggunakan mode mobile
+
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
-const question = (text) => new Promise((resolve) => rl.question(text, resolve));
 let owner = JSON.parse(fs.readFileSync('./src/owner.json'));
 
 // API for autoresponse
@@ -26,31 +24,32 @@ global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.
 
 const DataBase = require('./src/database');
 const database = new DataBase();
+
 (async () => {
-	const loadData = await database.read();
-	if (loadData && Object.keys(loadData).length === 0) {
-		global.db = {
-			sticker: {},
-			users: {},
-			groups: {},
-			database: {},
-			settings: {},
-			others: {},
-			...(loadData || {}),
-		};
-		await database.write(global.db);
-	} else {
-		global.db = loadData;
-	}
-	
-	setInterval(async () => {
-		if (global.db) await database.write(global.db);
-	}, 30000);
+    const loadData = await database.read();
+    if (loadData && Object.keys(loadData).length === 0) {
+        global.db = {
+            sticker: {},
+            users: {},
+            groups: {},
+            database: {},
+            settings: {},
+            others: {},
+            ...(loadData || {}),
+        };
+        await database.write(global.db);
+    } else {
+        global.db = loadData;
+    }
+
+    setInterval(async () => {
+        if (global.db) await database.write(global.db);
+    }, 30000);
 })();
 
 const { GroupUpdate, GroupParticipantsUpdate, MessagesUpsert, Solving } = require('./src/message');
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif');
-const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/function');
+const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, sleep } = require('./lib/function');
 
 console.log(chalk.cyan(figlet.textSync("XLICON-V4", {
     font: 'DOS Rebel',
@@ -68,13 +67,8 @@ console.log(chalk.white.bold(`${chalk.gray.bold("📃  Information :")}
 
 ${chalk.green.bold("Powered By XLICON BOTZ")}\n`));
 
-
-
 // Start XliconBot
 async function startXliconBot() {
-    let version = [2, 3000, 1015901307];
-    let isLatest = false;
-
     const { state, saveCreds } = await useMultiFileAuthState(`./session`);
     const msgRetryCounterCache = new NodeCache();
     
@@ -86,76 +80,52 @@ async function startXliconBot() {
             creds: state.creds,
             keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
         },
-        version, 
+        version: [2, 3000, 1015901307],
         markOnlineOnConnect: true,
         generateHighQualityLinkPreview: true,
-        getMessage: async (key) => {
-            let jid = jidNormalizedUser(key.remoteJid);
-            let msg = await store.loadMessage(jid, key.id);
-            return msg?.message || "";
-        },
         msgRetryCounterCache,
-        defaultQueryTimeoutMs: undefined,
     });
    
     store.bind(XliconBotInc.ev);
 
-    if (pairingCode && !XliconBotInc.authState.creds.registered) {
-        if (useMobile) throw new Error('Cannot use pairing code with mobile API');
-
-        let phoneNumber;
-        phoneNumber = await question('Please enter your number starting with 92 :\n');
-        phoneNumber = phoneNumber.trim();
-
-        setTimeout(async () => {
-            const code = await XliconBotInc.requestPairingCode(phoneNumber);
-            console.log(chalk.black(chalk.bgGreen(`🎁  Pairing Code : ${code}`)));
-        }, 3000);
-    }
-
-    store.bind(XliconBotInc.ev);
-    await Solving(XliconBotInc, store);
     XliconBotInc.ev.on('creds.update', saveCreds);
+
     XliconBotInc.ev.on('connection.update', async (update) => {
-        const { connection, lastDisconnect, receivedPendingNotifications } = update;
+        const { connection, lastDisconnect } = update;
         if (connection === 'close') {
             const reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-            if (reason === DisconnectReason.connectionLost) {
-                console.log('Connection to Server Lost, Attempting to Reconnect...');
-                startXliconBot();
-            } else if (reason === DisconnectReason.connectionClosed) {
-                console.log('Connection closed, Attempting to Reconnect...');
-                startXliconBot();
-            } else if (reason === DisconnectReason.restartRequired) {
-                console.log('Restart Required...');
-                startXliconBot();
-            } else if (reason === DisconnectReason.timedOut) {
-                console.log('Connection Timed Out, Attempting to Reconnect...');
-                startXliconBot();
-            } else if (reason === DisconnectReason.badSession) {
-                console.log('Delete Session and Scan again...');
-                process.exit(1);
-            } else if (reason === DisconnectReason.connectionReplaced) {
-                console.log('Close current Session first...');
-                XliconBotInc.logout();
-            } else if (reason === DisconnectReason.loggedOut) {
-                console.log('Scan again and Run...');
-            } else if (reason === DisconnectReason.Multidevicemismatch) {
-                console.log('Scan again...');
-            } else {
-                XliconBotInc.end(`Unknown DisconnectReason : ${reason}|${connection}`);
+            switch (reason) {
+                case DisconnectReason.connectionLost:
+                    console.log('Connection to Server Lost, Attempting to Reconnect...');
+                    startXliconBot();
+                    break;
+                case DisconnectReason.connectionClosed:
+                    console.log('Connection closed, Attempting to Reconnect...');
+                    startXliconBot();
+                    break;
+                case DisconnectReason.restartRequired:
+                    console.log('Restart Required...');
+                    startXliconBot();
+                    break;
+                case DisconnectReason.timedOut:
+                    console.log('Connection Timed Out, Attempting to Reconnect...');
+                    startXliconBot();
+                    break;
+                case DisconnectReason.badSession:
+                    console.log('Delete Session and Scan again...');
+                    process.exit(1);
+                    break;
+                default:
+                    console.log('Unknown DisconnectReason:', reason);
             }
-        }
-        if (connection == 'open') {
-            console.log('Connected to : ' + JSON.stringify(XliconBotInc.user, null, 2));
-        } else if (receivedPendingNotifications == 'true') {
-            console.log('Please wait About 1 Minute...');
+        } else if (connection === 'open') {
+            console.log('Connected to:', JSON.stringify(XliconBotInc.user, null, 2));
         }
     });
     
     XliconBotInc.ev.on('contacts.update', (update) => {
         for (let contact of update) {
-            let id = XliconBotInc.decodeJid(contact.id);
+            const id = XliconBotInc.decodeJid(contact.id);
             if (store && store.contacts) store.contacts[id] = { id, name: contact.notify };
         }
     });
